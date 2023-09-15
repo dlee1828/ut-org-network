0028ba54afcebe25af9cd9dad38cffed0e719a45
Daniel Lee <daniel8lee58@gmail.com>
2023-09-15 13:54:34 -0500

    set up nextui for frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,16 @@
 'use client'
-
+import { Button, NextUIProvider } from "@nextui-org/react";
 import Image from 'next/image'
-import styles from './page.module.css'
-import { useEffect } from 'react'
 
 export default function Home() {
   return (
-    <div>Hi</div>
+    <NextUIProvider>
+      <div className="flex flex-col items-center pt-12">
+        <h1>UT Org Network</h1>
+        <Button>Log In</Button>
+        <Button>Sign Up</Button>
+        <Button>Explore</Button>
+      </div>
+    </NextUIProvider>
   )
 }
