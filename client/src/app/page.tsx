c19fa36126b41bb26afcc40aba23de29e67c42ee
Daniel Lee <daniel8lee58@gmail.com>
2023-09-20 14:07:35 -0500

    set up sign-in page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,17 @@
 'use client'
-import { Button, NextUIProvider } from "@nextui-org/react";
+import { Button, Card, NextUIProvider } from "@nextui-org/react";
 import Image from 'next/image'
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <NextUIProvider>
-      <div className="flex flex-col items-center pt-12">
-        <h1>UT Org Network</h1>
+      <div className="flex flex-col items-center pt-12 gap-8">
+        <h1 className="text-5xl font-bold leading-relaxed py-1 bg-gradient-to-r from-orange-400 to-orange-600 text-transparent bg-clip-text">UT Org Network</h1>
         <Button>Log In</Button>
-        <Button>Sign Up</Button>
+        <Button onClick={() => router.push('/sign-up')}>Sign Up</Button>
         <Button>Explore</Button>
       </div>
     </NextUIProvider>
---
+++ client/src/app/sign-up/page.tsx
@@ -0,0 +1,63 @@
+'use client'
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Input, Select, SelectItem } from "@nextui-org/react";
+import { useState } from "react";
+import { collegeMajors, slugify } from "./college-majors";
+
+export default function SignUp() {
+  type EnrollmentStatus = "undergraduate" | "graduate"
+  const [enrollmentStatus, setEnrollmentStatus] = useState<EnrollmentStatus | null>(null);
+  type Year = "freshman" | "sophomore" | "junior" | "senior"
+  const [year, setYear] = useState<Year | null>(null);
+  const [major, setMajor] = useState<string | null>(null);
+
+  return <div className="flex flex-col items-center pt-12">
+    <Card className="w-96">
+      <CardHeader className="text-center">
+        Sign Up
+      </CardHeader>
+      <Divider />
+      <CardBody className="flex gap-4">
+        <Input width="100%" placeholder="First Name" />
+        <Input width="100%" placeholder="Last Name" />
+        <Input width="100%" type="email" placeholder="Email" />
+        <Input width="100%" type="password" placeholder="Password" />
+        <Select onChange={(e) => setEnrollmentStatus(e.target.value as EnrollmentStatus)} label="Enrollment Status">
+          <SelectItem key="undergraduate" value="undergraduate">
+            Undergraduate Student
+          </SelectItem>
+          <SelectItem key="graduate" value="graduate">
+            Graduate Student
+          </SelectItem>
+        </Select>
+        {
+          enrollmentStatus == 'undergraduate' ? <Select onChange={(e) => setYear(e.target.value as Year)} label="Year">
+            <SelectItem key="freshman" value="freshman">
+              Freshman
+            </SelectItem>
+            <SelectItem key="sophomore" value="sophomore">
+              Sophomore
+            </SelectItem>
+
+            <SelectItem key="junior" value="junior">
+              Junior
+            </SelectItem>
+
+            <SelectItem key="senior" value="senior">
+              Senior
+            </SelectItem>
+          </Select> : null
+        }
+        <Select onChange={(e) => setMajor(e.target.value)} label="Major">
+          {
+            collegeMajors.map((major) =>
+              <SelectItem key={slugify(major)} value={slugify(major)}>{major}</SelectItem>
+            )
+          }
+        </Select>
+        <Button>
+          Sign Up
+        </Button>
+      </CardBody>
+    </Card>
+  </div>
+}
