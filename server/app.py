7c6ae27c4480c2fa4a99a6b88139ae941dd256df
Daniel Lee <daniel8lee58@gmail.com>
2023-11-13 16:39:43 -0600

    edits

--- server/app.py
+++ server/app.py
@@ -8,8 +8,13 @@ import networkx as nx
 
 G = nx.read_graphml('./data/orgnetwork.graphml')
 
+G_dgl, id_key = org_net.clean_graph_pipeline(G) # Create synthetic graph and pass to the clean pipeline
+inv_graph = org_net.invert_graph(G_dgl, save=True)
+
+model = org_net.train_pipeline(G_dgl, epochs=300)
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "tried read_graphml"
+    return "added the other stuff"
