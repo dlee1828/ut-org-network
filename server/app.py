e7ce97fd17ff920282ddcf579072278de5d566ee
Daniel Lee <daniel8lee58@gmail.com>
2023-11-13 13:49:40 -0600

    edits

--- server/app.py
+++ server/app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-import numpy as np
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    s = str(np.array([1, 2, 3]))
-    return s
+    return "added stuff to requirements.txt"
