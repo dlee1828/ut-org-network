2adeeabe74db9a8621dbfa661e8bf85e90c53f02
Daniel Lee <daniel8lee58@gmail.com>
2023-11-13 16:35:06 -0600

    edits

--- server/app.py
+++ server/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 import sys
-sys.path.append('../src')
-sys.path.append('..')
+# sys.path.append('../src')
+# sys.path.append('..')
 
 import org_net 
+import networkx as nx
+
+G = nx.read_graphml('./data/orgnetwork.graphml')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "tried adding a few imports"
+    return "tried read_graphml"
