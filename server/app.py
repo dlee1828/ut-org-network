1d1cd789d67c55b3ad901c8a4e8f80529193c51c
Daniel Lee <daniel8lee58@gmail.com>
2023-11-13 13:46:00 -0600

    edits

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import numpy as np
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Test after merging'
+    s = str(np.array([1, 2, 3]))
+    return s
